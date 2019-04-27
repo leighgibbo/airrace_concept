6c812e41fc1e1f5336bfeb5c140cf574de7f9f73
Leigh Gibson <leigh.gibson01@gmail.com>
2019-04-27 13:41:53 +0930

    swapped over to vanilla-tilt (tilt.js fork) as it had support for device gyroscope

--- js/main.js
+++ js/main.js
@@ -29,18 +29,23 @@ $(function () { // wait for document ready
 
 
 	/* TiltJS */
-	$('.this-tilts').tilt({
-		maxTilt: 10,
-		perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
-		easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-		// scale: 1.01,      // 2 = 200%, 1.5 = 150%, etc..
-		speed: 300,    // Speed of the enter/exit transition.
-		transition: false,   // Set a transition on enter/exit.
-		disableAxis: null,   // What axis should be disabled. Can be X or Y.
-		reset: false,   // If the tilt effect has to be reset on exit.
-		// glare: false,  // Enables glare effect
-		// maxGlare: 0.3       // From 0 - 1.
-	})
+	const tilt = document.querySelector('.this-tilts');
+	VanillaTilt.init(tilt,{
+		max: 10
+	});
+	
+	// $('.this-tilts').tilt({
+	// 	maxTilt: 10,
+	// 	perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+	// 	easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+	// 	// scale: 1.01,      // 2 = 200%, 1.5 = 150%, etc..
+	// 	speed: 300,    // Speed of the enter/exit transition.
+	// 	transition: false,   // Set a transition on enter/exit.
+	// 	disableAxis: null,   // What axis should be disabled. Can be X or Y.
+	// 	reset: false,   // If the tilt effect has to be reset on exit.
+	// 	// glare: false,  // Enables glare effect
+	// 	// maxGlare: 0.3       // From 0 - 1.
+	// })
 
 
 	/* Window Panning calculations - TODO: functionize */
